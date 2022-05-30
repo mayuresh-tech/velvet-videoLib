d7bcc9c192976c916f86ec3a0e25f041612d9179
mayuresh-tech <mmayuresh12@gmail.com>
2022-05-30 20:52:36 +0530

    feat: added navbar and homepage

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react";
+
+import "./Navbar.css";
 
 const Navbar = () => {
+  return (
+    <nav class="nav-box-bar desktop-nav">
+      <div id="nav-section-bar">
+        <div id="logo-bar">
+          <p class="logo-text">
+            <span>Velvet</span> Videos
+          </p>
+        </div>
+      </div>
 
-}
+      <div class="login-nav-box">
+        <div id="social-links-bar">
+          <button class="no-bg hov user-icon">
+            <i class="fa-solid fa-user"></i>
+          </button>
+          <button class="no-bg hov btn-solid-primary">Login</button>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
